045ec34def65ea5e67073790777d14f841a64024
Sampath Kumar GM <msampathkumar@gmail.com>
2022-01-06 14:26:08 -0800

    commiting for case correction

--- src/components/store/index.js
+++
@@ -1,7 +0,0 @@
-import { applyMiddleware, createStore } from "redux";
-import reduxLogger from "redux-logger";
-import GithubReducer from "./reducers/GithubReducer";
-
-const store = createStore(GithubReducer, applyMiddleware(reduxLogger));
-
-export default store;
